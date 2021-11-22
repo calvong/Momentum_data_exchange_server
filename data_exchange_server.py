f67f121d65271ed74472e167752b5d6a5ba4134d
calvong <calvongchi@gmail.com>
2021-11-22 14:29:14 +1100

    bug fixed: included iron python to path

--- data_exchange_server.py
+++ data_exchange_server.py
@@ -1,14 +1,24 @@
+import sys
+
+# adding Iron Python to path
+sys.path.append(r'C:\Program Files\IronPython 3.4')
+sys.path.append(r'C:\Program Files\IronPython 3.4\Lib')
+
 import socket
 import csv
+import os
 
 
 class DataExchangeServer:
     def __init__(self):
-        sock_ip = 'localhost'
+        sock_ip = '192.137.137.5'
         sock_port = 8888
         self._sock = socket.socket()
-        #self._sock.close()
+
         self._sock.connect((sock_ip, sock_port))
+
+        # get current file path
+        self._file_base_path = os.path.dirname(os.path.abspath(__file__))
 
     def send_data(self, mmt_data):
         """Receive fake Momentum data (1 or 2) and read data from file to send out for analysis
@@ -29,9 +39,9 @@ class DataExchangeServer:
         msg = cmd_response.split(delim)
         is_outlier = msg[0]
         temperature = msg[1]
-        print(f"is outlier = {is_outlier}")
-        print(f"Temperature {temperature}")
-        return is_outlier, temperature
+        # print(f"is outlier = {is_outlier}")
+        # print(f"Temperature {temperature}")
+        return int(bool(is_outlier)), temperature
 
     def _get_fake_data(self, data_set):
         """Construct a fake data string msg according to the data set number
@@ -39,13 +49,13 @@ class DataExchangeServer:
         :param data_set: data set number
         :return: whole data set in string
         """
-        data_set1_path = 'HPLC_data/Result_Export2909.csv'
-        data_set2_path = 'HPLC_data/Result_Export2889.csv'
+        data_set1_path = '/HPLC_data/Result_Export2909.csv'
+        data_set2_path = '/HPLC_data/Result_Export2889.csv'
 
         if data_set == 1:
-            data_path = data_set1_path
+            data_path = self._file_base_path + data_set1_path
         elif data_set == 2:
-            data_path = data_set2_path
+            data_path = self._file_base_path + data_set2_path
 
         ts = []
         data = []
@@ -68,7 +78,7 @@ class DataExchangeServer:
         return data_msg
 
 
-if __name__ == '__main__':
-    exchange_server = DataExchangeServer()
-
-    exchange_server.send_data(mmt_data=2)
+# if __name__ == '__main__':
+#     exchange_server = DataExchangeServer()
+#
+#     exchange_server.send_data(mmt_data=1)
